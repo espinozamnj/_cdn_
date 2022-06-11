33bf45bcdfb5a18cf0d695d4b09142a094f4d82b
spain lap <1052179@senati.pe>
2022-06-11 17:50:31 -0500

    cdn

--- getlogin.js
+++ getlogin.js
@@ -1,6 +1,7 @@
 (function(){
     let ready = false
     function add_login() {
+        ready = true
         const _k = 'RI6hHXQ6T2BvRh7YT9DgQbQJGrelaoEdVkR7UZQG9/FWvZILB0mSPjXQeNd+THGUpQGD/dNZsxMNu1YfLsmMY4fQmt8NAQUgXcpn4bUiwEqxRUx/ebNwkWwFkq+dhZj0mq6vIOcIHYbEteATq3Mcn9FHC3AcadjrvlnjZHbocgr2WGf0wLx+HtE5jWlgiBL/dUtEKayhysBxzYe5LYDewZXP63sKjYMZBj63T/61Vb8Ppb5YtifQRMWstnQXF7dV/9nivSPYjwRJ5vNcsN9B5k/XnJXzpeHhLWn4iSka8ojRxvytnoqzXvU/B+hZUDc8+vVv3FNrNM/dubj8bM178BlcMUQ0Lwkxnj91XUoftdBHDhmB+Q/fi2Pb6iApMdTCLFi2j1VisApSodqRr1Y2ltiOkvlEkLp0fDcU8/SgyK2jkra6tUepUHYomXFENQd/HlK2R7glBX55Xn/2JRrbJwLO/68oP1VsECAB6a3+uR1vbEVtiEXbz1IDv2WiGiSuipdt4vnQeGvO4FaY7d4TO3DMpPGFLHtkmNNPE/Qd0AAe849d5A2AJrGrfd0HSjfcerELmKW+U5JVcS5vfvnl5DJmVGH7K+LILQ6EvkpjBt0rW5IfFQoDZ5FX0lr4BfrkB6ksJhd9ZVJTdjFw7MLwtbgItf/cFADMOy2NxCwNxtRFO1j5eILhXqEp+xCoF99eRaD6AEHdR3rEEdoh9hzXF10htKNdTKCZsPDkh9ip10yAJrfE/sp0CXC6Jxxz+vgRKl5SxpbU/VBFrEgTXg0GzvDI5GIxqMhtCxGaRvO8c5RdQoZnjrcBFyW34Z3sMsF+jZ6GSwnqoDSQ5gUC12f1kop3nv/WMl/PMLT3oL5w/3hIn5iPaKl69gFx/I5mnYcMy1WHt2mk5wHtR7z3Mr1hUzZGAGm3w8ru2HQz9cpA1GZyHkYBXaNdJsefKZlwE9WOe+w7j0dlg5O5dMNlj7kQdzYgIRFXOAGed5MIsRxHSl3RxkoFSjaOV2ULLmUG3GB1iayS4VpcnlzrX30Alpbo+o2CiNTLKCgyhPcS4PZJF0y99oH7KSQVdjOvM7P9muXjCvxTuj7Dqs7fUJyi6T8YRRNHqtFAqOb8EqNBBJuP2nSXlaKfKU3IXlsst9eGmhazXETebzUWTIf9KVVPDltHnhk9fYPS6r012N2J9hoBFtiUKZemn/nYjBpLbXZIvHnQChl+qmsARwSdMvCUg2TYH6OJmPg4DzRFHIYKRpT7d83djCUfC2uHBnWc34gz6YyBVIfl2xXJr0VGqkyRsrr2kd25aSiUYvt1XxW8Rzi7zNbxeCTgEhONnsyzoqM8OMpVQk4mh1wMj1Jt2/pNVpf2I/4aBXPyCmYDE4Xy5An23Fclo+z3xt2Gj5x/MjrMIJV0kIfpN97Y+BgCwsp+61ywPkNuCWrhyVOKvn0YfBELk8S5Zn6Un8Nepa4sRKPdQHJH88wltaahqnDnC/YEJF953SBwdGVDDT7d227umGFfO3QsCM4JYWHPGe7Xw9ON/e76T965Vnyj39Y2C43vR7xJUzu2j4EkNN1GJgy8gNRTyThMk0L+7wG9E5RF8EN0i2CX+y63Gz6oJTDrtu3ozUmj+YbF3qFZkTJU9uqk2tzrB20j0uq+82rb0SaRQMm0mP9907emQd1IxARYoik0R0AT2DLSl+PzK/B1nVNHjAX0OPBABgIjuMNq6AUn7ILcRov+QJJggWHxUrgC9oxysXyqDieNPzct7VtvEmolTaFp6Lp97ZN0wIJN/Oh83C+s0hTqCroje+dFboZ86qac3wQdwtpW5LhLREZn5F0tXexSqQuiBuySDHEUugHdKSQB07QoMXirppl9vpuBk/xDBpYKVvL9JDQLpcfnMPsDWKLTwpyUds8kgtdZJl4YRGe9hbdZQdeWtYP4bDXo/HPP7b4R4y5liCHrx6WC1vPDeHroZ2e45WS1h+E77s1SfsXrWFImzLX9ldE48dL01Nmf5Mv4yZumCUJLi6rpW6EyU+x3KCnEiLOPZQhgEsE1LhVJohb/'
         let v_r = Math.floor(Math.random() * (9 - 0 + 1) + 0),
             eB = document.getElementById('getlogin')
@@ -28,11 +29,9 @@
             if (isNaN(g) || gl > 7 || gl < 5){
                 __e.p.value = ''
                 __e.p.setAttribute('placeholder', 'NaN')
-                setTimeout(
-                    function (){
-                        __e.p.setAttribute('placeholder', v_r)
-                    }, 1e3
-                )
+                setTimeout(function (){
+                    __e.p.setAttribute('placeholder', v_r)
+                }, 1e3)
             } else {
                 let _t = document.createElement('input'), v = {w: 0}
         
@@ -73,47 +72,29 @@
         })
         let cl = function(){
             __e.p.value = '1111111'
-            setTimeout(
-                function() {
-                    __e.b.click()
-                }, 2e2
-            )
+            setTimeout(function() {
+                __e.b.click()
+            }, 2e2)
         }
         __e.p.addEventListener('keydown',function(v) {
-                if ((v.which || v.keyCode || v.charCode) == 13 && v.shiftKey) {
-                    __e.b.click()
-                }
+            if ((v.which || v.keyCode || v.charCode) == 13 && v.shiftKey) {
+                __e.b.click()
             }
-        )
+        })
         window.document.addEventListener('keydown',function(v) {
-                if (v.shiftKey && v.keyCode == 73) {
-                    alert(v_r + 1)
-                    __e.p.focus()
-                    setTimeout(
-                        function() {
-                            __e.p.value = ''
-                        }, 2e2
-                    )
-                }
-                if (v.shiftKey && v.keyCode == 8) {cl()}
+            if (v.shiftKey && v.keyCode == 73) {
+                alert(v_r + 1)
+                __e.p.focus()
+                setTimeout(function() {
+                    __e.p.value = ''
+                }, 2e2)
             }
-        )
+            if (v.shiftKey && v.keyCode == 8) {cl()}
+        })
         __e.b.addEventListener('dblclick', function(){cl()})
     }
-    window.addEventListener('load', function(){
-        add_login()
-        ready = true
-    })
-    setTimeout(function(){
-        if (!ready) {
-            add_login()
-            ready = true
-        }
-    }, 1e3)
-    setTimeout(function(){
-        if (!ready) {
-            add_login()
-            ready = true
-        }
-    }, 5e3)
+    function checkAddReady(){if(!ready){add_login()}}
+    window.addEventListener('load', function() {checkAddReady()})
+    setTimeout(function(){checkAddReady()}, 1e3)
+    setTimeout(function(){checkAddReady()}, 5e3)
 })()
