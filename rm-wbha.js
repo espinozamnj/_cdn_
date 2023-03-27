b8e4a66adf6a69f2acb32f8744e0cb148284dad8
espinozamnj <1025179@senati.pe>
2023-03-26 21:36:29 -0500

    cdn

--- rm-wbha.js
+++ rm-wbha.js
@@ -6,14 +6,14 @@ var __cDe = (salt, encoded) => {
     .map((hex) => parseInt(hex, 16))
     .map(applySaltToChar)
     .map((charCode) => String.fromCharCode(charCode))
-    .join("")
+    .join('')
 }
 var __cEn = (salt, text) => {
   const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0))
   const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2)
   const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code)
   
-  return text.split("").map(textToChars).map(applySaltToChar).map(byteHex).join("")
+  return text.split('').map(textToChars).map(applySaltToChar).map(byteHex).join('')
 }
 (function(){
   let dir_project = '_j.em'
